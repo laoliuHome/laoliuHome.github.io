8c51ff39f038b6de18ec0b8696f1cc92d933a6ab
laoliuHome <huangfangliu123@163.com>
2022-07-13 21:16:52 +0800

    Site updated: 2022-07-13 21:16:52

---
+++ js/comments.js
@@ -0,0 +1,18 @@
+Waline.init({
+		  el: '#waline',
+		  serverURL: 'https://waline-laoliuhome.vercel.app',
+		  pageview: true,
+		  emoji: [
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-emoji',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-body',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-food',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-natural',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-object',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-symbol',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-people',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-sport',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-travel',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-weather',
+			'https://unpkg.com/@waline/emojis@1.0.1/tw-flag',
+  		  ],
+		});
